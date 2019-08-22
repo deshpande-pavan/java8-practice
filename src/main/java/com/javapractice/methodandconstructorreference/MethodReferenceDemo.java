4345f92651ea23ac0caba347044fe5f1c179f811
deshpande-pavan <pavandeshpande60@gmail.com>
2019-08-22 11:19:26 +0530

    method reference with normal method

--- src/main/java/com/javapractice/methodandconstructorreference/MethodReferenceDemo.java
+++ src/main/java/com/javapractice/methodandconstructorreference/MethodReferenceDemo.java
@@ -2,17 +2,18 @@ package com.javapractice.methodandconstructorreference;
 
 public class MethodReferenceDemo {
 
-	public static void m1() {
+	public void m1() {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Child Thread");
 		}
 	}
 
 	public static void main(String[] args) {
-		Runnable r = MethodReferenceDemo::m1;
+		MethodReferenceDemo methodDemo = new MethodReferenceDemo();
+		Runnable r = methodDemo::m1;
 		Thread t = new Thread(r);
 		t.start();
-		for(int i=0;i<10;i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println("main thread");
 		}
 	}
