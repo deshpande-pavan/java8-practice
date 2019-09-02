7e8952452a22ca07ea4865d88ab565f16f9f9c56
deshpande-pavan <pavandeshpande60@gmail.com>
2019-09-02 18:00:24 +0530

    added some hackerrank java questions

--- src/main/java/com/javapractice/interviewprograms/JavaDateAndTime.java
+++ src/main/java/com/javapractice/interviewprograms/JavaDateAndTime.java
@@ -1,7 +1,16 @@
 package com.javapractice.interviewprograms;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class JavaDateAndTime {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int date = sc.nextInt();
+		int month = sc.nextInt();
+		int year = sc.nextInt();
+		LocalDate newdate = LocalDate.of(year, month, date);
+		System.out.println(newdate.getDayOfWeek().toString());
+		sc.close();
 	}
 }
