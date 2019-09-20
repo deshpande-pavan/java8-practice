c2ce183880ac3b9a13f602b2a28b2c13f5ba627e
deshpande-pavan <pavandeshpande60@gmail.com>
2019-09-20 13:26:25 +0530

    startsWith(), Arrays.stream(), filter() in streams

--- src/main/java/com/javapractice/streams/IntStreamDemo.java
+++ src/main/java/com/javapractice/streams/IntStreamDemo.java
@@ -1,5 +1,6 @@
 package com.javapractice.streams;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -8,7 +9,8 @@ public class IntStreamDemo {
 	public static void main(String[] args) {
 		IntStream.range(1, 10).skip(5).forEach(System.out::println);
 		System.out.println(IntStream.range(1, 5).sum());
-		String[] arr = { "Pavan", "Prashant", "Pradeep" };
+		String[] arr = { "Pavan", "Prashant", "Pradeep", "Sandeep" };
 		Stream.of(arr).sorted().findFirst().ifPresent(x -> System.out.println(x));
+		Arrays.stream(arr).filter(x -> x.startsWith("P")).sorted().forEach(System.out::print);
 	}
 }
