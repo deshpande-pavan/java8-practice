b1d4864219506bed1422fb15f7b1e29343014099
deshpande-pavan <pavandeshpande60@gmail.com>
2019-09-20 13:21:23 +0530

    Instream,sorted(), findFirst() in streams

--- src/main/java/com/javapractice/streams/IntStreamDemo.java
+++ src/main/java/com/javapractice/streams/IntStreamDemo.java
@@ -1,11 +1,14 @@
 package com.javapractice.streams;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class IntStreamDemo {
 
 	public static void main(String[] args) {
 		IntStream.range(1, 10).skip(5).forEach(System.out::println);
 		System.out.println(IntStream.range(1, 5).sum());
+		String[] arr = { "Pavan", "Prashant", "Pradeep" };
+		Stream.of(arr).sorted().findFirst().ifPresent(x -> System.out.println(x));
 	}
 }
