92caa67899cbd8f9172f50642a3ec3dd85b7b2df
deshpande-pavan <pavandeshpande60@gmail.com>
2019-09-20 13:13:58 +0530

    Instream,sum() in streams

--- src/main/java/com/javapractice/streams/IntStreamDemo.java
+++ src/main/java/com/javapractice/streams/IntStreamDemo.java
@@ -6,5 +6,6 @@ public class IntStreamDemo {
 
 	public static void main(String[] args) {
 		IntStream.range(1, 10).skip(5).forEach(System.out::println);
+		System.out.println(IntStream.range(1, 5).sum());
 	}
 }
